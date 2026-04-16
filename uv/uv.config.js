3576652c7d68179fdc94b6d90e98238112fe8134
ZoomMods1 <hkpyssyt@gmail.com>
2026-04-16 05:06:47 -0400

    Update bare URL in uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://uv.student-services.tech/bare/', 
+    bare: 'https://v6.bare.duckdns.org/', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
